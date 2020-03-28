18c13ed3d495460eb458259b3fd3b79955e68b4c
Vincent Scavetta <vstro24@gmail.com>
2020-03-28 18:10:42 -0400

    Setting up the angular project for styles

--- AnimalCrossingTracker/src/app/app.component.ts
+++ AnimalCrossingTracker/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AnimalCrossingTracker';
+
+  private MIN_WIDTH = 1000;
+  private innerWidth: number;
+  isOpened: boolean;
+  isMobile: boolean;
+  searchBox: string;
+
+  sideNavMode: string;
+  DESKTOP_SIDENAV = 'side';
+  MOBILE_SIDENAV = 'over';
+
+  constructor(
+  ) { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.sizeCheck();
+  }
+
+  sizeCheck() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth < this.MIN_WIDTH) {
+      this.sideNavMode = this.MOBILE_SIDENAV;
+      this.isOpened = false;
+      this.isMobile = true;
+    } else {
+      this.sideNavMode = this.DESKTOP_SIDENAV;
+      this.isOpened = true;
+      this.isMobile = false;
+    }
+  }
+
+  ngOnInit() {
+    this.sizeCheck();
+  }
 }
--- AnimalCrossingTracker/src/app/app.module.ts
+++ AnimalCrossingTracker/src/app/app.module.ts
@@ -1,16 +1,93 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatStepperModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+  MatNativeDateModule
+} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
